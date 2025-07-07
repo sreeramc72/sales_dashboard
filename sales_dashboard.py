f76c71dc1322b9d7565bb8478f326cf39fa63f4d
sreeramc72 <sreeramchowdary54@gmail.com>
2025-07-07 20:45:35 +0000

    chore: allow secrets.toml to be tracked for development/demo purposes

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -4,7 +4,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import mysql.connector
-from google.cloud import bigquery
 import numpy as np
 from datetime import datetime, timedelta
 import seaborn as sns
